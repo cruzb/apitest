f02d26f661b5ca1f70b925932f666d6714a05f70
cruzb <cruzb1010@gmail.com>
2020-12-25 17:58:40 -0800

    refactor and simplify

--- index.js
+++ index.js
@@ -1,37 +1,29 @@
 const express = require('express');
 const body_parser = require('body-parser');
-const logger = require('morgan');
 const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+
+
 const app = express();
+const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
-const NODE_ENV = process.env.NODE_ENV || 'development';
 
-app.set('port', PORT);
-app.set('env', NODE_ENV);
+const pathname = path.join(__dirname, 'routes');
+fs.readdirSync(pathname).forEach(file => {
+    file = path.resolve(pathname, file);
+    if(fs.statSync(file).isFile()) {
+        console.log(pathname + file);
+        app.use('/', require(`${pathname}/${file}`));
+    }
+})
 
-app.use(logger('tiny'));
-app.use(body_parser.json());
-
-app.use('/', require(path.join(__dirname, 'routes', 'get.js')));
 app.use((req, res, next) => {
   const err = new Error(`${req.method} ${req.url} Not Found`);
   err.status = 404;
   next(err);
 });
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(err.status || 500);
-  res.json({
-    error: {
-      message: err.message,
-    },
-  });
-});
-app.listen(PORT, () => {
-  console.log(
-    `Express Server started on Port ${app.get(
-      'port'
-    )} | Environment : ${app.get('env')}`
-  );
-});
+
+
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
